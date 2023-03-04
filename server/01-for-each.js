6e03ae47921cfcec0f647a6954b35901000b5c36
Chris <christophersanchezwolf005@gmail.com>
2023-03-04 16:45:34 -0400

    se realizo la clase de forEach, se realizo un pequeno reto de lista de tareas

---
+++ server/01-for-each.js
@@ -0,0 +1,14 @@
+const letters=['a','b','c'];
+
+//for(let index=0;index<letters.length;index++){
+   // const element=letters[index];
+  //  console.log(element);
+//}
+
+//La forma en que yo lo  hago
+/* for(element of letters){
+    console.log(element)
+}
+ */
+// Con foreEach()
+letters.forEach(item=>console.log(item))
